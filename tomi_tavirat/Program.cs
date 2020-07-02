8de11175f4b6b992051a8e43a3fc5b22bfafedae
varzoltan <64203391+varzoltan@users.noreply.github.com>
2020-07-02 14:28:40 +0200

    14:28

--- tomi_tavirat/Program.cs
+++ tomi_tavirat/Program.cs
@@ -3,13 +3,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace tomi_tavirat
 {
     class Program
     {
+        struct Adat
+        {
+            public string telepules;
+            public string ido;
+            public string szelirany;
+            public int fok;
+        }
         static void Main(string[] args)
         {
+            Adat[] adatok = new Adat[500];
+            StreamReader olvas = new StreamReader(@"C:\Users\Rendszergazda\Desktop\2020_majus\tavirathu13.txt");
+            int n = 0;
+            while (!olvas.EndOfStream)
+            {
+                string sor = olvas.ReadLine();
+                string[] db = sor.Split();
+                adatok[n].telepules = db[0];
+                adatok[n].ido = db[1];
+                adatok[n].szelirany = db[2];
+                adatok[n].fok = int.Parse(db[3]);
+                n++;
+            }
+            olvas.Close();
+            Console.WriteLine("1.Feladat: Beolvasás kész!");
+            Console.WriteLine("Kérem adja meg egy város kódját:");
+            string varos = Console.ReadLine();
+            
+            Console.ReadKey();
         }
     }
 }
