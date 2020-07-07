eb9a695bd98109cf3c548ef38219d9472c724674
varzoltan <64203391+varzoltan@users.noreply.github.com>
2020-07-07 10:06:29 +0200

    10:06

--- tomi_tavirat/Program.cs
+++ tomi_tavirat/Program.cs
@@ -33,8 +33,59 @@ namespace tomi_tavirat
             }
             olvas.Close();
             Console.WriteLine("1.Feladat: Beolvasás kész!");
-            Console.WriteLine("Kérem adja meg egy város kódját:");
+            Console.Write("2.Feladat: Kérem adja meg egy város kódját:");
             string varos = Console.ReadLine();
+            string ido = null;
+            for (int i = n-1; i>-1;i--)
+            {
+                if (varos == adatok[i].telepules)
+                {
+                    ido = adatok[i].ido;
+                    Console.WriteLine("{0}:{1}-kor volt az utolsó mérés",ido.Substring(0,2),ido.Substring(2,2));
+                    break;
+                }
+            }
+
+            //3.feladat
+            int max = adatok[0].fok;
+            int min = adatok[0].fok;
+            int h = 0;
+            int g = 0;
+            for (int i = 1; i<n;i++)
+            {
+                if (max < adatok[i].fok)
+                {
+                    max = adatok[i].fok;
+                    h = i;
+                }           
+            }
+
+            for (int i = 1; i < n; i++)
+            {
+                if (min > adatok[i].fok)
+                {
+                    min = adatok[i].fok;
+                    g = i;
+                }
+            }
+            Console.WriteLine("A legmagasabb hőmérséklet: {0} {1}:{2} {3} fok",adatok[h].telepules, adatok[h].ido.Substring(0,2), adatok[h].ido.Substring(2, 2), adatok[h].fok);                                
+            Console.WriteLine("A legalacsonyabb hőmérséklet: {0} {1}:{2} {3} fok", adatok[g].telepules, adatok[g].ido.Substring(0, 2), adatok[g].ido.Substring(2, 2), adatok[g].fok);
+
+            //4.feladat
+            int u = 0;
+            for (int i =0;i <n;i++)
+            {
+                if (adatok[i].szelirany == "00000")
+                {
+                    Console.WriteLine(adatok[i].telepules+" " + adatok[i].ido.Substring(0,2)+":"+ adatok[i].ido.Substring(2,2));
+                    u++;
+                }
+                
+            }
+            if (u ==0)
+            {
+                Console.WriteLine("Nem volt szélcsend a a mérések idején.");
+            }
             
             Console.ReadKey();
         }
